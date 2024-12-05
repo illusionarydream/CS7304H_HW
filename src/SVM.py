557b82191d24f06dbde36de3485cfbcf7aeca8c2
illusionarydream <illusionary@sjtu.edu.cn>
2024-12-05 15:38:39 +0800

    basic SVM

--- src/SVM.py
+++ src/SVM.py
@@ -1,41 +1,131 @@
-from sklearn import svm
-from sklearn.datasets import make_classification
-from sklearn.model_selection import train_test_split
-import pandas as pd
-
+import numpy as np
+from tqdm import tqdm
+import cvxpy as cp
 from dataloader import DataLoader
+
+import matplotlib.pyplot as plt
+
+
+class SVM:
+    def __init__(self, features, labels, kernel_type: str = "linear"):
+        self.features = features
+        self.labels = labels
+        self.vector_dim = features.shape[1]
+        self.n_samples = features.shape[0]
+        self.class_dim = max(labels) + 1
+        self.kernel_type = kernel_type
+
+        # one vs rest
+        self.weights = np.zeros((self.class_dim, self.vector_dim))
+        self.bias = np.zeros(self.class_dim)
+
+    # * multi-class SVM
+    def one_vs_rest(self):
+        for i in range(self.class_dim):
+            labels = self.labels.copy()
+
+            # set the label of the i-th class to 1 and the others to -1
+            for j in range(self.n_samples):
+                if labels[j] == i:
+                    labels[j] = 1
+                else:
+                    labels[j] = -1
+
+            # apply one vs one
+            self.weights[i], self.bias[i] = self.one_vs_one(
+                self.features, labels)
+
+    # * two-class SVM
+    def kernel(self, X):
+
+        if self.kernel_type == "linear":
+            return X @ X.T
+
+        elif self.kernel_type == "rbf":
+            gamma = 1 / self.vector_dim
+            return np.exp(-gamma * np.linalg.norm(X[:, None] - X, axis=2) ** 2)
+
+        else:
+            raise ValueError("Invalid kernel type")
+
+    def one_vs_one(self, features, labels):
+        # initialize variables
+        alphas = cp.Variable(self.n_samples)
+
+        # kernel matrix
+        K = self.kernel(features)
+        Q = np.outer(labels, labels) * K
+        Q += 1E-5 * np.eye(self.n_samples)
+
+        # objective function
+        objective = cp.Maximize(cp.sum(alphas) - 0.5 * cp.quad_form(alphas, Q))
+        # constraints
+        constraints = [alphas >= 0, labels @ alphas == 0]
+
+        # solve the problem
+        problem = cp.Problem(objective, constraints)
+        problem.solve()
+
+        # result
+        alpha_values = alphas.value
+        support_vector_indices = np.where(alpha_values > 1e-5)[0]
+
+        # calculate weights and bias
+        weights = np.sum(alpha_values[support_vector_indices]
+                         * labels[support_vector_indices, None]
+                         * features[support_vector_indices], axis=0)
+        bias = np.mean(labels[support_vector_indices]
+                       - np.dot(features[support_vector_indices], weights))
+
+        return weights, bias
+
+    def print_info(self):
+        print('features shape:', self.features.shape)
+        print('labels shape:', self.labels.shape)
+        print('vector dim:', self.vector_dim)
+        print('class dim:', self.class_dim)
+        print('kernel type:', self.kernel_type)
+
+
+def plot_data(X, y, w, b):
+
+    # plot data
+    plt.scatter(X[:, 0], X[:, 1], c=y)
+    # plot y = wx + b
+    x = np.linspace(0, 4, 100)
+    y = (-w[0] * x - b) / w[1]
+    plt.plot(x, y, '-r')
+
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('SVM Classification')
+    plt.show()
 
 
 if __name__ == '__main__':
 
-    # get data
-    train_file_path = 'datasets/train_feature.pkl'
-    train_label_file_path = 'datasets/train_labels.npy'
-    eval_file_path = 'datasets/test_feature.pkl'
+    # # get data
+    # train_file_path = 'datasets/train_feature.pkl'
+    # train_label_file_path = 'datasets/train_labels.npy'
+    # eval_file_path = 'datasets/test_feature.pkl'
 
-    data_loader = DataLoader(
-        train_file_path,
-        train_label_file_path,
-        eval_file_path)
+    # data_loader = DataLoader(
+    #     train_file_path,
+    #     train_label_file_path,
+    #     eval_file_path)
 
-    data_loader.print_info()
+    # # build SVM model
+    # svm = SVM(data_loader.train_features, data_loader.train_labels)
 
-    # build model
-    clf = svm.SVC(kernel='linear')
+    # svm.print_info()
 
-    # classify on the training data
-    clf.fit(data_loader.train_features, data_loader.train_labels)
+    X = np.array([[1.0, 3.0], [1.0, 1.0], [2.0, 3.0], [3.0, 4.0], [3.0, 2.0]])
+    y = np.array([1, 1, 0, 0, 0])
 
-    # evaluate on the training data
-    accuracy = clf.score(data_loader.train_features, data_loader.train_labels)
+    svm = SVM(X, y)
+    svm.one_vs_rest()
 
-    # predict on the eval data
-    eval_predictions = clf.predict(data_loader.eval_features)
+    print(svm.weights)
+    print(svm.bias)
 
-    # save predictions to a CSV file
-    df = pd.DataFrame({
-        'ID': range(len(eval_predictions)),
-        'label': eval_predictions
-    })
-    df.to_csv('test_predictions.csv', index=False)
-    print('Predictions saved to test_predictions.csv')
+    plot_data(X, y, svm.weights[0], svm.bias[0])
--- src/dataloader.py
+++ src/dataloader.py
@@ -3,18 +3,43 @@ import numpy as np
 
 
 class DataLoader:
-    def __init__(self, train_file_path, train_label_file_path, eval_file_path, split_ratio=0.2):
+    def __init__(self, train_file_path, train_label_file_path, eval_file_path, random_seed=42):
 
-        self.train_features = pickle.load(open(train_file_path, 'rb'))
-        self.train_labels = np.load(train_label_file_path)
+        self.origin_features = pickle.load(open(train_file_path, 'rb'))
+        self.origin_labels = np.load(train_label_file_path)
         self.eval_features = pickle.load(open(eval_file_path, 'rb'))
 
+        self.train_features = None
+        self.train_labels = None
+        self.test_features = None
+        self.test_labels = None
+
+        self.random_seed = random_seed
+        np.random.seed(self.random_seed)
+        self.random_shuffle()
+        self.random_split()
+
+    def random_shuffle(self):
+        # shuffle the data
+        indices = np.arange(self.origin_features.shape[0])
+        np.random.shuffle(indices)
+        self.origin_features = self.origin_features[indices]
+        self.origin_labels = self.origin_labels[indices]
+
+    def random_split(self, split_ratio=0.2):
         # split the training data into training and evaluation sets
-        # split_index = int(len(original_train_features) * (1 - split_ratio))
+        split_index = int(self.origin_features.shape[0] * (1 - split_ratio))
+
+        self.train_features = self.origin_features[:split_index]
+        self.train_labels = self.origin_labels[:split_index]
+        self.test_features = self.origin_features[split_index:]
+        self.test_labels = self.origin_labels[split_index:]
 
     def print_info(self):
         print('Train features shape:', self.train_features.shape)
         print('Train labels shape:', self.train_labels.shape)
+        print('Test features shape:', self.test_features.shape)
+        print('Test labels shape:', self.test_labels.shape)
         print('Evaluate features shape:', self.eval_features.shape)
 
 
